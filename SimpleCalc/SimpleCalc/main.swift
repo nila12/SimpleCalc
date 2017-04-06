52cd061a4c0323fcfc96b9387f65dfbec289a88b
Mathi Manavalan <mathimano@hotmail.com>
2017-04-06 12:51:45 -0700

    added error messages

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -12,14 +12,14 @@
  user can enter a number, hit return
  program calculates the result
  
- can also enter series of numbers separated by spaces and followed 
-    by either count or avg
+ can also enter series of numbers separated by spaces and followed
+ by either count or avg
  
  can also enter a single number followed by a space and the word fact
-    to receive the factorial of that number
+ to receive the factorial of that number
  */
 
-// EXTRA CREDIT 
+// EXTRA CREDIT
 // once inside folder containing main.swift, run following commands to execute in terminal
 // swiftc ./main.swift
 // ./main
@@ -39,31 +39,30 @@ while true {
     if (responseArr.count > 1) { //listed nums
         
         switch responseArr[responseArr.count - 1] {
-            case "count" :
+        case "count" :
             
-                print ("Result: \(numArr.count)")
-            case "avg" :
+            print ("Result: \(numArr.count)")
+        case "avg" :
             
-                var sum = numArr.reduce(0, +)
+            var sum = numArr.reduce(0, +)
             
-                print ("Result \(sum / numArr.count)")
+            print ("Result: \(sum / numArr.count)")
             
-            case "fact" :
-                if numArr.count > 1 {
-                    print("mismatching values and function")
-                } else {
-                    var sum = 1
-                    for index in 2...numArr[0] {
-                        sum *= index
-                    }
-                    
-                    print ("Result: \(sum)")
+        case "fact" :
+            if numArr.count > 1 {
+                print("mismatching values and function")
+            } else {
+                var sum = 1
+                for index in 2...numArr[0] {
+                    sum *= index
                 }
+                
+                print ("Result: \(sum)")
+            }
             
-            default :
-                print ("invalid function")
+        default :
+            print ("invalid function")
         }
-        
         
     } else {
         
@@ -72,7 +71,6 @@ while true {
         }
         
         var first = UInt.init(response)!
-        
         var oper = readLine(strippingNewline: true)!
         var sec = UInt(readLine(strippingNewline: true)!)!
         
